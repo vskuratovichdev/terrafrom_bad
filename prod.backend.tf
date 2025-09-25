7b633bee6ba5985c27fe39eb5f7e6dec3091b61a
vskuratovichLT <VSkuratovich@lt.life>
2025-09-25 16:28:19 +0300

    Update Azure backend configuration in prod.backend.tf
    - Changed resource group name to "prd-terraform-state-rg-01".
    - Updated storage account name to "prdfstate".

--- prod.backend.tf
+++ prod.backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "prd-sw-noteserve-terraform-state-rg-01"
-    storage_account_name = "prdnoteservetfstate"
+    resource_group_name  = "prd-terraform-state-rg-01"
+    storage_account_name = "prdfstate"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
