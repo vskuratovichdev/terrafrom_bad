fd63e03e3b4676a1cbb4536f038775253f29f168
vskuratovichLT <VSkuratovich@lt.life>
2025-09-26 12:34:10 +0300

    Remove test backend configuration from Terraform files

--- test.backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "noteserve-terraform-state-rg-01"
-    storage_account_name = "noteserveterraformstate"
-    container_name       = "tfstate"
-    key                  = "test.terraform.tfstate"
-  }
-}
